0a79a923ad3691b4bec7ade19f9bc1b17ee261a0
luqmanito <luqmangrahit@gmail.com>
2023-01-28 06:59:25 +0700

    adddb

--- config.js
+++ config.js
@@ -1,12 +1,27 @@
-require("dotenv").config()
-const {PORT, DB_PASSWORD} = process.env
-const {Pool} = require("pg")
-const postgreDB = new Pool({
-    host: "localhost",
-    user: "postgres",
-    database: "postgres",
-    password: DB_PASSWORD,
-    port : PORT
-})
+require("dotenv").config();
+const {
+  PORT,
+  DB_PASSWORD,
+  DB_HOST_DEV,
+  DB_USER_DEV,
+  DB_NAME_DEV,
+  DB_PASS_DEV,
+} = process.env;
+const { Pool } = require("pg");
+// const postgreDB = new Pool({
+//     host: "localhost",
+//     user: "postgres",
+//     database: "postgres",
+//     password: DB_PASSWORD,
+//     port : PORT
+// })
 
-module.exports = postgreDB
+const postgreDB = new Pool({
+  host: DB_HOST_DEV,
+  user: DB_USER_DEV,
+  database: DB_NAME_DEV,
+  password: DB_PASS_DEV,
+  port: PORT,
+});
+
+module.exports = postgreDB;
